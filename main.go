802114e5d215f20d4d89021299483ada8ec26323
Hardik <hardikm9850@gmail.com>
2025-11-25 16:09:35 +0530

    removed code doc

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import "Zero/cmd"
 
-// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
 	cmd.Execute()
 }
